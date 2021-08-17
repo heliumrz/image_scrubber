3c8396633622ef49ed52f108929b848da8a778e3
Rui Zhan <zhanrz@amazon.com>
2021-08-17 00:45:12 -0700

    Add image loader

--- src/App.js
+++ src/App.js
@@ -6,28 +6,42 @@ import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <AmplifySignOut />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+function App() {
+  const [images, setImages] = useState([])
+  useEffect(() => {
+    fetchImages()
+  }, [])
+  async function fetchImages() {
+    // Fetch list of images from S3
+    let s3images = await Storage.list('')
+    // Get presigned URL for S3 images to display images in app
+    s3images = await Promise.all(s3images.map(async image => {
+      const signedImage = await Storage.get(image.key)
+      return signedImage
+    }))
+    setImages(s3images)
   }
+  function onChange(e) {
+    if (!e.target.files[0]) return
+    const file = e.target.files[0];
+    // upload the image then fetch and rerender images
+    Storage.put(uuid(), file).then (() => fetchImages())
+  }
+
+  return (
+      <div>
+        <h1>Photo Album</h1>
+        <span>Add new image</span>
+        <input
+            type="file"
+            accept='image/png'
+            onChange={onChange}
+        />
+        <div style={{display: 'flex', flexDirection: 'column'}}>
+          { images.map(image => <img src={image} style={{width: 400, marginBottom: 10}} />) }
+        </div>
+      </div>
+  );
 }
 
 export default withAuthenticator(App);
